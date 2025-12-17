e94c485218e021f4f2375a69aea4381ed29ee84f
mouandjorichard50-hue <mouandjorichard50@gmail.com>
2025-12-17 02:58:09 +0100

    Refactor database URI configuration
    Updated database URI configuration to use os.path.join for better path handling.

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from datetime import datetime
 app = Flask(__name__)
 app.secret_key = 'cle_secrete_scolarite_2025_finale'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecole_v3.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/ecole_v3.db'
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'ecole_v3.db')
 
 db = SQLAlchemy(app)
 
@@ -224,5 +226,6 @@ if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
 
